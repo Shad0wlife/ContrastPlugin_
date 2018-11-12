32633cc2f0afdf8546304efc9c2504b42c77f113
Shad0wlife <matze_skywalker@hotmail.de>
2018-11-12 19:40:28 +0100

    Code Cleanup, remove unneccessary checks and attributes
    Removed unneccessary attributes, moved them to return values of methods.
    
    Also removed multiple checks on the same value with the same purpose.

--- src/ContrastPlugin_.java
+++ src/ContrastPlugin_.java
@@ -11,8 +11,6 @@ public class ContrastPlugin_  implements PlugInFilter {
 	/*Attribute*/
 	private int valueMin = BLACK;
 	private int valueMax = WHITE;
-	private int newMin = BLACK;
-	private int newMax = WHITE;
 	
 	@Override
 	public int setup(String arg, ImagePlus imp) {
@@ -21,85 +19,84 @@ public class ContrastPlugin_  implements PlugInFilter {
 
 	@Override
 	public void run(ImageProcessor ip) {
-		//Teil 1: Nutzereinstellungen abfragen und verarbeiten
-		getSettings(ip);
+		//Nutzereinstellungen abfragen und verarbeiten
+		int newMin = BLACK;
+		int newMax = WHITE;
+		double saturation = requestSaturation();
+		
+		//Prüft ob ungültiger Sättigungswert (nicht in [0,50])
+		if(saturation < 0 || saturation >= 50) {
+			if(saturation != IJ.CANCELED) {
+				IJ.error("Falscher Grenzwert!", "Der eingegebene Grenzwert befindet sich außerhalb des Intervalls [0,50).");
+			}
+			newMin = requestMinLimit();
+			newMax = requestMaxLimit();
+			
+			//Prüft ob ungültiger Grenzwert (nicht in [0,255])
+			if(newMin < 0 || newMin > 255 || newMax < 0 || newMax > 255 || newMin >= newMax) {
+				IJ.error("Falscher Grenzwert!", "Mindestens einer der eingegebenen Grenzwerte befindet sich außerhalb des Intervalls [0,255]"
+						+ "oder das Minimum ist größer gleich dem Maximum");
+				//Programmabbruch
+				return;
+			}
+			
+			findMinMax(ip);
+		}else {
+			findMinMaxSaturated(ip, saturation);
+		}
 		
 		//Überprüfe ob der ermittelte Maximalwert kleiner oder gleich dem Minimalwert ist.
 		//Wenn ja, beende, da sonst das Einfarbige (oder leere) Bild einfach nur schwarz wird.
 		if(this.valueMax <= this.valueMin) {
 			IJ.error("Bild ist einfarbig oder ein Fehler ist aufgetreten!");
+			//Programmabbruch
 			return;
 		}
 		
-		//Teil 2: Bild verarbeiten
-		modify(ip);
-	}
-	
-	/**
-	 * Verarbeitet die Abfrage der Plugin-Einstellungen
-	 * @param ip Der ImageProcessor vom Bild
-	 */
-	private void getSettings(ImageProcessor ip) {
-		double saturation = requestSaturation();
-		
-		//Prüft ob umgültiger Sättigungswert (-1)
-		if(saturation == -1) {
-			requestLimits();
-			findMinMax(ip);
-		}else {
-			findMinMaxSaturated(ip, saturation);
-		}
+		modifyImage(ip, newMin, newMax);
 	}
 	
 	/**
 	 * Führt die eigentliche Kontrastveränderung auf Basis der gewählten Einstellungen durch
 	 * @param ip Der ImageProcessor vom Bild
 	 */
-	private void modify(ImageProcessor ip) {
+	private void modifyImage(ImageProcessor ip, int newMinValue, int newMaxValue) {
 		int width = ip.getWidth();
 		int height = ip.getHeight();
-		double factor = (double)(newMax - newMin)/(double)(valueMax-valueMin);
+		double factor = (double)(newMaxValue - newMinValue)/(double)(valueMax-valueMin);
 		
 		//Iterate over all pixels and modify value
 		for(int cntWidth = 0; cntWidth < width; cntWidth++) {
 			for(int cntHeight = 0; cntHeight < height; cntHeight++) {
 				//Formel zur Berechnung des neuen Werts
-				int pixelVal = (int)(newMin + (ip.get(cntWidth, cntHeight) - valueMin) * factor);
+				int pixelVal = (int)(newMinValue + (ip.get(cntWidth, cntHeight) - valueMin) * factor);
 				ip.putPixel(cntWidth, cntHeight, clamp(pixelVal, 0, 255));
 			}
 		}
 	}
 	
-
 	/**
-	 * Die Nutzerabfrage für die neuen Grenzwerte des Bilds (falls kein Sättigungswert angegeben wurde).
+	 * Die Nutzerabfrage für den Minimalwert
+	 * @return Der eingegebene Wert
 	 */
-	private void requestLimits() {
-		int newMin = (int)IJ.getNumber("Bitte einen neuen Minimalwert eingeben:", 0.0);
-		int newMax = (int)IJ.getNumber("Bitte einen neuen Maximalwert eingeben:", 255.0);
-		
-		if(newMin < 0 || newMin > 255 || newMax < 0 || newMax > 255) {
-			IJ.error("Falscher Grenzwert!", "Mindestens einer der eingegebenen Grenzwerte befindet sich außerhalb des Intervalls [0,255].");
-			return;
-		}
-		
-		this.newMin = newMin;
-		this.newMax = newMax;
+	private int requestMinLimit() {
+		return (int)IJ.getNumber("Bitte einen neuen Minimalwert eingeben:", 0.0);
 	}
 	
+	/**
+	 * Die Nutzerabfrage für den Maximalwert
+	 * @return Der eingegebene Wert
+	 */
+	private int requestMaxLimit() {
+		return (int)IJ.getNumber("Bitte einen neuen Maximalwert eingeben:", 255.0);
+	}
 	
 	/**
-	 * Die Nutzerabfrage für den Sättigungsanteil [0,50).
-	 * @return
+	 * Die Nutzerabfrage für den Sättigungsanteil
+	 * @return Der eingegebene Wert
 	 */
 	private double requestSaturation() {
-		double saturation = IJ.getNumber("Bitte einen Saettigungswert in Prozent eingeben:", 0.0);
-		
-		if(saturation < 0 || saturation >= 50) {
-			IJ.error("Falscher Grenzwert!", "Der eingegebene Grenzwert befindet sich außerhalb des Intervalls [0,50).");
-			return -1;
-		}
-		return saturation;
+		return IJ.getNumber("Bitte einen Saettigungswert in Prozent eingeben:", 0.0);
 	}
 	
 	/**
